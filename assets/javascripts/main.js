3ef6d76cbb3fe265d751d69520f74ea95f56bd30
KeybrL <keyboard-w@outlook.com>
2018-10-14 22:11:45 +0800

    移除部分不合适的代码

--- assets/javascripts/main.js
+++ assets/javascripts/main.js
@@ -1,7 +1,6 @@
 $(window).ready(function() {
     $(window).resize(function() {
         $('.dialog').each(function() {
-            console.log('hhh');
             $(this).css({
                 'top': ($(window).height() - $(this).height()) / 2 + 'px',
                 'left': ($(window).width() - $(this).width()) / 2 + 'px'
